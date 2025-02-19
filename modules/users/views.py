064ad0ca54ce18628ba8ffada6a0551ad6684bfb
MoonPrincess06 <sara.garg@web.de>
2025-02-20 00:02:49 +0100

    feat: added username in login response

--- modules/users/views.py
+++ modules/users/views.py
@@ -3,7 +3,17 @@ from rest_framework import permissions
 from rest_framework.generics import CreateAPIView
 
 from modules.users.serializers import UserSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data['username'] = self.user.username
+        return data
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
 
 class CreateUser(CreateAPIView):
     model = User
